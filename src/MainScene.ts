09082a2e1c75f58f651296b5824298e82c0c01d7
Andris Grishan <andris.grishan@gmail.com>
2020-02-21 17:59:23 +0200

    scene test

--- src/MainScene.ts
+++ src/MainScene.ts
@@ -18,15 +18,6 @@ export class MainScene {
         var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 3, 25, new BABYLON.Vector3(0, 0, 4.5), this._scene);
         camera.attachControl(this._canvas, true);
 
-        // // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
-        // this._camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), this._scene);
-
-        // // Target the camera to scene origin.
-        // this._camera.setTarget(BABYLON.Vector3.Zero());
-
-        // // Attach the camera to the canvas.
-        // this._camera.attachControl(this._canvas, false);
-
         // Create a basic light, aiming 0,1,0 - meaning, to the sky.
         this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this._scene);
 
@@ -36,18 +27,27 @@ export class MainScene {
 
         // up boxes
         for (let i = -5; i < 5; i++) {
-            let box = BABYLON.MeshBuilder.CreateBox("box", { height: 0.2, width: 2, depth: 2 }, this._scene);
-            box.position.y = 0.2;
-            box.position.x = 2.2 * i + 1;
+            let box = BABYLON.MeshBuilder.CreateBox(
+                "box",
+                {
+                    height: 0.4, 
+                    width: 2, 
+                    depth: 2
+                }, 
+                this._scene
+            );
+
+            box.position.y = 0.4;
+            box.position.x = 2.01 * i + 1;
             box.position.z = 2;
             box.material = cubeMaterial;
         }
 
         // lower boxes
         for (let i = -5; i < 5; i++) {
-            let box = BABYLON.MeshBuilder.CreateBox("box", { height: 0.2, width: 2, depth: 2 }, this._scene);
-            box.position.y = 0.2;
-            box.position.x = 2.2 * i + 1;
+            let box = BABYLON.MeshBuilder.CreateBox("box", { height: 0.4, width: 2, depth: 2 }, this._scene);
+            box.position.y = 0.4;
+            box.position.x = 2.01 * i + 1;
             box.position.z = -2;
             box.material = cubeMaterial;
         }
